fa331705948cca58dab71cb23564d27309ca9a42
Alen Vochanec <alen.vochanec@gmail.com>
2025-05-08 15:56:22 +0200

    Home and features

--- src/components/home/home-page.tsx
+++ src/components/home/home-page.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { createClient } from '@/utils/supabase/client';
 import { useUserInfo } from '@/hooks/useUserInfo';
 import '../../styles/home-page.css';
-import { LocalizationBanner } from '@/components/home/header/localization-banner';
+//import { LocalizationBanner } from '@/components/home/header/localization-banner';
 import Header from '@/components/home/header/header';
 import { HeroSection } from '@/components/home/hero-section/hero-section';
 import { Pricing } from '@/components/home/pricing/pricing';
@@ -14,13 +14,13 @@ import { TestimonialsSlider } from '@/components/testimonials/testimonials';
 import { BenefitsShowcase } from '@/components/benefits/benefits';
 import { ROICalculator } from '@/components/roi-calculator/roi-calculator';
 import { LiveUsageStatistics } from '@/components/statistics/statistics';
-import { TransformationSlider } from '@/components/transformation-slider/transformation-slider';
+//import { TransformationSlider } from '@/components/transformation-slider/transformation-slider';
 import { Footer } from '@/components/home/footer/footer';
 
 export function HomePage() {
   const supabase = createClient();
   const { user } = useUserInfo(supabase);
-  const [country, setCountry] = useState('US');
+  const [country] = useState('US');
 
   return (
     <>
